9a88e668b12aa77264537718a3edb3a68a7f5030
Seth <sethvance26@gmail.com>
2021-05-06 23:18:46 -0400

    Adding base notes to dbjson

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
 
+
+
+
+app.use(express.static("public"));
+//This is a route to the index.html. Where the user goes initially. 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
+//This is a route to the notes.html. 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
+
+
+
+
+
+
+
+
 
 //This code starts our server ~
 app.listen(PORT, () => {
