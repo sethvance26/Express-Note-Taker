3b7df6c1c21393bb19df9cace06370f8f80d778a
Seth <sethvance26@gmail.com>
2021-05-07 22:20:23 -0400

    Attempting to try to get a line to delete

--- Develop/server.js
+++ Develop/server.js
@@ -54,6 +54,10 @@ app.post("/api/notes", (req, res) => {
 });
 });
 
+// app.delete('/api/notes/:id', (req, res) => {
+    
+
+//   fs.readFile(path.join(__dirname, "/db/db.json"), function (err, response) {
 
 
 
